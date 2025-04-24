5de0d0d1121587fcb572c9d87891fc2f946d928b
aks-lijiaqing <74410648+Lijiaqing233@users.noreply.github.com>
2025-04-25 02:30:32 +0900

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,30 @@
+﻿// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+
+    uint256 _favoriteNumber;
+
+    // This is a comment!
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 favoriteNumber) public {
+        _favoriteNumber = favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256){
+        return _favoriteNumber;
+    }
+
+    function addPerson(string memory name, uint256 favoriteNumber) public {
+        people.push(People(favoriteNumber, name));
+        nameToFavoriteNumber[name] = favoriteNumber;
+    }
+}
